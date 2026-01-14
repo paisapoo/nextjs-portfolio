65a15910ec7811c2ec7fe6e13cf9e112e7ef538e
pai sapoo <s.sapoo@thisisdmg.com>
2026-01-14 18:17:38 +0700

    update text

--- app/_section/hero_home.tsx
+++ app/_section/hero_home.tsx
@@ -3,8 +3,16 @@ export default function HeroHome() {
     <>
       <section className='h-[50vh] md:min-h-screen flex items-center bg-[url("/_assets/background/bg-blue.png")] bg-fill bg-center py-20 lg:py-40'>
         <div className='container  mx-auto px-4 text-center'>
-          <h1 className='font-jua font-bold text-white text-4xl md:text-8xl/32 '>
-            Welcome to My Portfolio
+          <h1 className='font-jua font-bold  text-4xl md:text-8xl/32 '>
+            <span
+              className='block text-transparent mb-4'
+              style={{
+                WebkitTextStroke: '.5px rgba(255, 255, 255, 0.3)',
+              }}
+            >
+              Welcome to
+            </span>
+            <span className='block text-white'>My Portfolio</span>
           </h1>
         </div>
       </section>
